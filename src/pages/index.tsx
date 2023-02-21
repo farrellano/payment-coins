bc02fb79c374b705dbf91fd276baeb5630fad478
farellano@cleverit.cl <farellano@cleverit.cl>
2023-02-21 15:11:39 -0300

    fix ref HtmlInputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,40 @@
-import React,{useRef,useEffect,useState} from 'react';
-import { useDimensions } from '@chakra-ui/react'
-import Header from '../components/head'
-import ConnectWallet from '../components/wallet'
-import Products from '../components/productsList'
-import {
-  Box
-} from "@chakra-ui/react";
+import React, { useRef, useEffect, useState } from "react";
+import { useDimensions } from "@chakra-ui/react";
+import Header from "../components/head";
+import ConnectWallet from "../components/wallet";
+import Products from "../components/productsList";
+import { Box } from "@chakra-ui/react";
 export default function Home() {
-
-  const mainRef = useRef();
+  const mainRef = useRef<HTMLDivElement>(null);
   const dimensions = useDimensions(mainRef);
 
-  const [size, setSize] = useState('');
+  const [size, setSize] = useState("");
 
   useEffect(() => {
     if (dimensions) {
       let width = dimensions.borderBox.width;
       switch (true) {
         case width < 499:
-          setSize('base');
+          setSize("base");
           break;
         case width >= 499 && width < 696:
-          setSize('sm');
+          setSize("sm");
           break;
         case width >= 696 && width < 945:
-          setSize('md');
+          setSize("md");
           break;
         case width >= 945:
-          setSize('lg');
+          setSize("lg");
           break;
       }
     }
   }, [dimensions]);
 
-  
   return (
     <>
-    <Header />
-    <ConnectWallet /> 
-    <Box
+      <Header />
+      <ConnectWallet />
+      <Box
         w="100vw"
         h="100vh"
         display="flex"
@@ -48,8 +44,7 @@ export default function Home() {
         as="main"
       >
         <Products />
-    </Box>
-    
+      </Box>
     </>
-  )
+  );
 }
